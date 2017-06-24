dfaa92805b3bd20e7647ed7d1e895359dcd7b235
zidorov <zidorov@gmail.com>
2017-06-24 23:02:58 +0300

    model update with new classifier

--- bitcointalk_sentiment_classifier.py
+++ bitcointalk_sentiment_classifier.py
@@ -4,44 +4,38 @@ import json
 import getopt
 import sys
 import pickle
+import warnings
 
 def main(argv):
+    warnings.filterwarnings('ignore', category=DeprecationWarning)
     input_file = ''
     model_file = ''
-    vectorizer_file = ''
     output_folder = ''
     try:
-        opts, args = getopt.getopt(argv, "hi:m:v:f:")
+        opts, args = getopt.getopt(argv, "hi:m:f:")
     except getopt.GetoptError:
-        print('bitcointalk_sentiment_classifier.py -i <inputfile> -m <model> -v <vectorizer> -f <output folder>')
+        print('bitcointalk_sentiment_classifier.py -i <inputfile> -m <model> -f <output folder>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('bitcointalk_sentiment_classifier.py -i <inputfile> -m <model> -v <vectorizer> -f <output folder>')
+            print('bitcointalk_sentiment_classifier.py -i <inputfile> -m <model> -f <output folder>')
             sys.exit()
         elif opt == '-i':
             input_file = arg
         elif opt == '-m':
             model_file = arg
-        elif opt == '-v':
-            vectorizer_file = arg
         elif opt == '-f':
             output_folder = arg
 
-    classify(input_file,model_file,vectorizer_file,output_folder)
+    classify(input_file,model_file,output_folder)
 
-def classify(input_file,model_file,vectorizer_file,output_folder):
+def classify(input_file,model_file,output_folder):
 
     topic_df = pd.read_json(input_file,orient='index')
     with open(model_file,'rb') as file:
         model = pickle.load(file)
-    with open(vectorizer_file,'rb') as file:
-        tfidf = pickle.load(file)
 
-
-    topic_matrix = tfidf.transform(topic_df[3])
-    topic_df['Sentiment'] = model.predict_proba(topic_matrix)[:,1]
-    topic_df['Sentiment'] = topic_df['Sentiment'].apply(lambda x: 0 if x<0.33 else (1 if x<0.66 else 2))
+    topic_df['Sentiment'] = model.predict(topic_df[3])
 
     def checkTimeFormat(time_string):
         try:
